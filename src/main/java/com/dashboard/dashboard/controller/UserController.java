c4449805fde0f08bcbc579a5bfa436f485c24cc0
황광회 <jack7141@nate.com>
2024-09-15 01:32:09 +0900

    Member Id Retrieve API  init

--- src/main/java/com/dashboard/dashboard/controller/UserController.java
+++ src/main/java/com/dashboard/dashboard/controller/UserController.java
@@ -1,14 +1,19 @@
 package com.dashboard.dashboard.controller;
 
+import com.dashboard.dashboard.domain.Member;
+import com.dashboard.dashboard.repository.DataJPAMemberRepository;
+import com.dashboard.dashboard.services.MemberService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -18,20 +23,35 @@ import java.util.Map;
 @RequestMapping("/api/v1/users")
 @Tag(name = "사용자", description = "사용자 관련 API")
 public class UserController {
-//    @Operation(
-//            summary = "사용자 조회",
-//            description = "사용자를 조회한 정보를 반환함",
-//            responses = {
-//                    @ApiResponse(
-//                            responseCode = "200",
-//                            description = "조회된 사용자 정보를 반환함."
-//                    )
-//            }
-//    )
-//    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-//    public Map<String, String> getUser(
-//            @Parameter(description = "조회할 사용자의 아이디", example = "userid")
-//            @PathVariable(name = "id", required = true) String id
-//    ) {
-//    }
+    private final MemberService memberService; // 'final' 키워드 추가
+
+
+    @Operation(
+            summary = "사용자 조회",
+            description = "사용자를 조회한 정보를 반환함",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "조회된 사용자 정보를 반환함.",
+                            content = @Content(schema = @Schema(implementation = Member.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "사용자를 찾을 수 없음.",
+                            content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+                    )
+            }
+    )
+    @GetMapping("/{id}")
+    public ResponseEntity<Member> getUser(
+            @Parameter(description = "조회할 사용자의 아이디", example = "1")
+            @PathVariable(name = "id", required = true) Long id
+
+    ) {
+        Member member = memberService.getMemberById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("사용자를 찾을 수 없습니다."));
+
+        return ResponseEntity.ok(member);
+
+    }
 }
